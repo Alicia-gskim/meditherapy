d138b1bcee05d9d569fa1a2e4bc616d50bd74c6a
gskim <gskim@kimids.co.kr>
2020-11-11 11:29:13 +0900

    first commit

---
+++ src/main/java/com/pkg/meditherapy/MeditherapyApplication.java
@@ -0,0 +1,13 @@
+package com.pkg.meditherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditherapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeditherapyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkg/meditherapy/MeditherapyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkg.meditherapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditherapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
